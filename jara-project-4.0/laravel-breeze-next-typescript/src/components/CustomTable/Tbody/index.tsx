926ab8683b543709cf38623453e9c6c01e08e682
k_dei <k_dei@dpt-inc.co.jp>
2024-02-01 18:57:01 +0900

    laravel-nextjs login configuration and login page implementation.

--- jara-project-4.0/laravel-breeze-next-typescript/src/components/CustomTable/Tbody/index.tsx
+++
@@ -1,4 +0,0 @@
-const CustomTbody = ({ children }: { children: React.ReactNode }) => {
-  return <tbody>{children}</tbody>;
-};
-export default CustomTbody;
--- jara-project-4.0/laravel-breeze-next-typescript/src/types/User.ts
+++
@@ -1,9 +0,0 @@
-export interface UserType {
-  id: number
-  email: string
-  name: string
-  password: string
-  email_verified_at?: Date
-  created_at: Date
-  updated_at: Date
-}
