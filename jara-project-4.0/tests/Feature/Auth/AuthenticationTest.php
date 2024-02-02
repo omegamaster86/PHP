adbd4afd12dfadc87b9d5e43658ea1116cf52489
二村 貴紘 <t_futamura@dpt-inc.co.jp>
2024-02-02 18:56:13 +0900

    Update jara-project-4.0/tests/Feature/Auth/AuthenticationTest.php

--- jara-project-4.0/tests/Feature/Auth/AuthenticationTest.php
+++ jara-project-4.0/tests/Feature/Auth/AuthenticationTest.php
@@ -1,47 +1,113 @@
 <?php
 
-namespace Tests\Feature\Auth;
+/*************************************************************************
+ *  Project name: JARA
+ *  File name: AuthenticatedSessionController.php
+ *  File extension: .php
+ *  Description: This is the controller file to manage login request
+ *************************************************************************
+ *  Author: DEY PRASHANTA KUMAR
+ *  Created At: 2023/11/02
+ *  Updated At: 2023/12/04
+ *************************************************************************
+ *
+ *  Copyright 2023 by DPT INC.
+ *
+ ************************************************************************/
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+namespace App\Http\Controllers\Auth;
 
-class AuthenticationTest extends TestCase
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+
+class AuthenticatedSessionController extends Controller
 {
-    use RefreshDatabase;
-
-    public function test_users_can_authenticate_using_the_login_screen(): void
+    /**
+     * Display the login page view.
+     */
+    public function create(): View
     {
-        $user = User::factory()->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password',
-        ]);
-
-        $this->assertAuthenticated();
-        $response->assertNoContent();
+        // dd("test");
+        //return view('auth.login');
+        $token = csrf_token();
+        $hoge = (string) $token . "000000hoge";
+        return view('auth.login', ["hoge" => $hoge]);
     }
 
-    public function test_users_can_not_authenticate_with_invalid_password(): void
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
     {
-        $user = User::factory()->create();
+        //dd($request->all(), $request->session()->token(), csrf_token());
+        $request->authenticate();
 
-        $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'wrong-password',
-        ]);
+        $request->session()->regenerate();
 
-        $this->assertGuest();
+        //Get the status of registered user
+        $temp_password_flag = Auth::user()->temp_password_flag;
+        //return redirect('http://127.0.0.1:3000/login');
+        //Redirect the temporary registered user to the my password change page
+        if ($temp_password_flag) {
+            return redirect('user/password-change');
+        }
+        //Redirect the registered user to the my page
+        else {
+            return redirect('my-page');
+        }
+    }
+    //20240119 React連携 ログイン画面CSRFトークン生成
+    public function createCsrf()
+    {
+        //$token = $request->session()->token();
+        // $token = "aaaaaaaaaa";
+        $token = csrf_token();
+        echo($token);
+        echo("aaaaaaaaa");
+        var_dump($token);
+        $hoge = str_split($token);
+        $hoge = "aaa000aaa000" . (string) $token . "hhhhhhh99999999";
+        return  $hoge;
     }
 
-    public function test_users_can_logout(): void
+    //20240119 React連携 ログイン画面遷移
+    public function loginCheck(Request $request)
     {
-        $user = User::factory()->create();
+        //$request->session()->token();
+        $reqData = $request->all();
+        return  $reqData;
+        $request->authenticate();
 
-        $response = $this->actingAs($user)->post('/logout');
+        $request->session()->regenerate();
 
-        $this->assertGuest();
-        $response->assertNoContent();
+        //Get the status of registered user
+        $temp_password_flag = Auth::user()->temp_password_flag;
+
+        return $temp_password_flag;
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        // Logout Function
+        Auth::guard('web')->logout();
+
+        //Destroy current session
+        $request->session()->invalidate();
+
+        //Destroy current  token
+        $request->session()->regenerateToken();
+
+        //Redirect the logout user to the index page
+        return redirect('/');
     }
 }
