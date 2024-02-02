53c1414b85d1d171ba77d332710cd5da78846899
t_futamura <t_futamura@dpt-inc.co.jp>
2024-02-02 19:19:58 +0900

    20240202 commit

--- jara-project-4.0/tests/Feature/Auth/AuthenticationTest.php
+++ jara-project-4.0/tests/Feature/Auth/AuthenticationTest.php
@@ -1,14 +1,42 @@
 <?php
 
-namespace Tests\Feature\Auth;
+/*************************************************************************
+ *  Project name: JARA
+ *  File name: AuthenticatedSessionController.php
+ *  File extension: .php
+ *  Description: This is the controller file to manage login request
+ *************************************************************************
+ *  Author: DEY PRASHANTA KUMAR
+ *  Created At: 2023/11/02
+ *  Updated At: 2023/12/04
+ *************************************************************************
+ *
+ *  Copyright 2023 by DPT INC.
+ *
+ ************************************************************************/
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_login_screen_can_be_rendered(): void
+    {
+        $response = $this->get('/login');
+
+        $response->assertStatus(200);
+    }
 
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
@@ -20,7 +48,7 @@ class AuthenticationTest extends TestCase
         ]);
 
         $this->assertAuthenticated();
-        $response->assertNoContent();
+        $response->assertRedirect(RouteServiceProvider::HOME);
     }
 
     public function test_users_can_not_authenticate_with_invalid_password(): void
@@ -42,6 +70,6 @@ class AuthenticationTest extends TestCase
         $response = $this->actingAs($user)->post('/logout');
 
         $this->assertGuest();
-        $response->assertNoContent();
+        $response->assertRedirect('/');
     }
 }
