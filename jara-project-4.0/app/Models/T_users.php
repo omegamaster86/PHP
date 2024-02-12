9ecb9d2c34f7aab23db80fd71ba5666bcd02db65
t_futamura <t_futamura@dpt-inc.co.jp>
2024-02-12 14:25:29 +0900

    20240212 getUserDataで取得する情報を追加

--- jara-project-4.0/app/Models/T_users.php
+++ jara-project-4.0/app/Models/T_users.php
@@ -75,33 +75,103 @@ class T_users extends Authenticatable
                             );
         return $user_ids;
     }
-
-
+    //対象ユーザーの情報を取得する
     public function getUserData($targetUserId)
     {
-        $users = DB::select('select `user_id`, 
-                                `user_name`, 
-                                `mailaddress`, 
-                                `sex`, 
-                                `residence_country`, 
-                                `residence_prefecture`, 
-                                `date_of_birth`, 
-                                `height`, 
-                                `weight`, 
-                                `user_type`, 
-                                `photo`, 
-                                `password`, 
-                                `temp_password`, 
-                                `expiry_time_of_temp_password`, 
-                                `certification_number`, 
-                                `expiry_time_of_certification_number`, 
-                                `temp_password_flag`
-                                from t_users
-                                where delete_flag = 0
-                                and user_id = ?'
+        $users = DB::select('select 
+                                user.`user_id`
+                                ,user.`user_name`
+                                ,user.`mailaddress`
+                                ,user.`sex`
+                                ,user.`sex_name`
+                                ,user.`residence_country`
+                                ,user.`residenceCountryName`
+                                ,user.`residence_prefecture`
+                                ,user.`residencePrefectureName`
+                                ,user.`date_of_birth`
+                                ,user.`height`
+                                ,user.`weight`
+                                ,user.`user_type`
+                                ,trim("・" from CONCAT(is_administrator,
+                                        is_jara,
+                                        is_pref_boat_officer,
+                                        is_organization_manager,
+                                        is_player,
+                                        is_volunteer,
+                                        is_audience)) as `userTypeName`
+                                ,user.`photo`
+                                ,user.`password`
+                                ,user.`temp_password`
+                                ,user.`expiry_time_of_temp_password`
+                                ,user.`certification_number`
+                                ,user.`expiry_time_of_certification_number`
+                                ,user.`temp_password_flag`
+                                FROM
+                                (
+                                    select user.`user_id`
+                                    ,user.`user_name`
+                                    ,user.`mailaddress`
+                                    ,user.`sex`
+                                    ,sex.sex	as `sex_name`
+                                    ,user.`residence_country`
+                                    ,coun.country_name	as `residenceCountryName`
+                                    ,user.`residence_prefecture`
+                                    ,pref.pref_name	as `residencePrefectureName`
+                                    ,user.`date_of_birth`
+                                    ,user.`height`
+                                    ,user.`weight`
+                                    ,user.`user_type`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,2,1) = 1 then "管理者・"
+                                        else ""
+                                        end as `is_administrator`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,3,1) = 1 then "JARA・"
+                                        else ""
+                                        end as `is_jara`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,4,1) = 1 then "県ボ職員・"
+                                        else ""
+                                        end as `is_pref_boat_officer`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,5,1) = 1 then "団体管理者・"
+                                        else ""
+                                        end as `is_organization_manager`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,6,1) = 1 then "選手・"
+                                        else ""
+                                        end as `is_player`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,7,1) = 1 then "ボランティア・"
+                                        else ""
+                                        end as `is_volunteer`
+                                    ,case 
+                                        when SUBSTR(user.`user_type`,8,1) = 1 then "一般ユーザー（観客）"
+                                        else ""
+                                        end as `is_audience`
+                                    ,user.`photo`
+                                    ,user.`password`
+                                    ,user.`temp_password`
+                                    ,user.`expiry_time_of_temp_password`
+                                    ,user.`certification_number`
+                                    ,user.`expiry_time_of_certification_number`
+                                    ,user.`temp_password_flag`
+                                    FROM `t_users` user
+                                    left join `m_sex` sex
+                                    on user.sex = sex.sex_id
+                                    left join m_countries coun
+                                    on user.residence_country = coun.country_id
+                                    left join m_prefectures	pref
+                                    on user.residence_prefecture = pref.pref_id
+                                    WHERE 1=1
+                                    and user.delete_flag = 0
+                                    and sex.delete_flag = 0
+                                    and coun.delete_flag = 0
+                                    and pref.delete_flag = 0
+                                    and user_id = ?
+                                )user'
                                 ,[$targetUserId]
                             );
-        
         if(isset($users[0])){
             $users = $users[0];
         }
