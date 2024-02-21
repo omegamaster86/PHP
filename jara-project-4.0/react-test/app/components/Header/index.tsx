52c725772e55e62735eb494b3b78958f4d7782a7
h_makino <h_makino@dpt-inc.co.jp>
2024-02-21 18:33:56 +0900

    ヘッダーメニュー ユーザ種別対応

--- jara-project-4.0/react-test/app/components/Header/index.tsx
+++ jara-project-4.0/react-test/app/components/Header/index.tsx
@@ -16,6 +16,14 @@ const Header: FC = () => {
   const [playerId, setPlayerId] = useState<number>(); //選手ID 20240221
   const [volunteerId, setVolunteerId] = useState<number>(); //ボランティアID 20240221
 
+  const [is_administrator, set_is_administrator] = useState<number>(); //管理者権限 20240221
+  const [is_jara, set_is_jara] = useState<number>(); //JARA権限 20240221
+  const [is_pref_boat_officer, set_is_pref_boat_officer] = useState<number>(); //県ボ権限 20240221  
+  const [is_organization_manager, set_is_organization_manager] = useState<number>(); //団体管理者権限 20240221
+  const [is_player, set_is_player] = useState<number>(); //選手権限 20240221
+  const [is_volunteer, set_is_volunteer] = useState<number>(); //ボランティア権限 20240221
+  const [is_audience, set_is_audience] = useState<number>(); //一般権限 20240221
+
   // メニューを開く
   const handleClick = (event: MouseEvent<HTMLButtonElement>, clickIndex: number) => {
     setAnchorEl(event.currentTarget);
@@ -78,6 +86,15 @@ const Header: FC = () => {
         console.log(playerInf.data.result[0]);
         setPlayerId(playerInf.data.result[0].player_id);
         setVolunteerId(playerInf.data.result[0].volunteer_id);
+
+        set_is_administrator(playerInf.data.result[0].is_administrator);
+        set_is_jara(playerInf.data.result[0].is_jara);
+        set_is_pref_boat_officer(playerInf.data.result[0].is_pref_boat_officer);
+        set_is_organization_manager(playerInf.data.result[0].is_organization_manager);
+        set_is_player(playerInf.data.result[0].is_player);
+        set_is_volunteer(playerInf.data.result[0].is_volunteer);
+        set_is_audience(playerInf.data.result[0].is_audience);
+
       } catch (error: any) {
         console.log(error);
       }
@@ -109,42 +126,63 @@ const Header: FC = () => {
             >
               大会検索
             </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/DummyMyPage');
-              }}
-              className='text-caption1'
-            >
-              大会結果管理
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/tournament?mode=create');
-              }}
-              className='text-caption1'
-            >
-              大会登録
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/DummyMyPage');
-              }}
-              className='text-caption1'
-            >
-              大会結果情報一括登録
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/DummyMyPage');
-              }}
-              className='text-caption1'
-            >
-              大会エントリー一括登録
-            </MenuItem>
+            {(is_administrator == 1
+              || is_jara == 1
+              || is_pref_boat_officer == 1
+              || is_organization_manager == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/DummyMyPage');
+                }}
+                className='text-caption1'
+              >
+                大会結果管理
+              </MenuItem>
+            ) : ''}
+
+            {(is_administrator == 1
+              || is_jara == 1
+              || is_pref_boat_officer == 1
+              || is_organization_manager == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/tournament?mode=create');
+                }}
+                className='text-caption1'
+              >
+                大会登録
+              </MenuItem>
+            ) : ''}
+            {(is_administrator == 1
+              || is_jara == 1
+              || is_pref_boat_officer == 1
+              || is_organization_manager == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/DummyMyPage');
+                }}
+                className='text-caption1'
+              >
+                大会結果情報一括登録
+              </MenuItem>
+            ) : ''}
+            {(is_administrator == 1
+              || is_jara == 1
+              || is_pref_boat_officer == 1
+              || is_organization_manager == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/DummyMyPage');
+                }}
+                className='text-caption1'
+              >
+                大会エントリー一括登録
+              </MenuItem>
+            ) : ''}
           </div>
         )}
         {/* clickIndexが1の時（選手情報押下時）に表示 */}
@@ -159,57 +197,70 @@ const Header: FC = () => {
             >
               選手検索
             </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/playerInformation?mode=create');
-              }}
-              className='text-caption1'
-            >
-              選手登録
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                const urlStr = '/playerInformation?mode=update' + '&player_id=' + playerId;
-                router.push(urlStr);
-              }}
-              className='text-caption1'
-            >
-              選手情報更新
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                const urlStr = '/playerInformationRef' + '?player_id=' + playerId;
-                router.push(urlStr);
-                // router.push('/playerInformation?mode=create');
-              }}
-              className='text-caption1'
-            >
-              選手情報参照
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                const urlStr = '/playerInformationRef?mode=delete' + '&player_id=' + playerId;
-                router.push(urlStr);
-                // router.push('/playerInformation?mode=create');
-              }}
-              className='text-caption1'
-            >
-              選手情報削除
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/playerInformation?mode=create');
-              }}
-              className='text-caption1'
-            >
-              選手一括登録
-            </MenuItem>
-
+            {(is_jara == 1
+              || is_pref_boat_officer == 1
+              || is_organization_manager == 1
+              || is_volunteer == 1
+              || is_audience == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/playerInformation?mode=create');
+                }}
+                className='text-caption1'
+              >
+                選手登録
+              </MenuItem>
+            ) : ''}
+            {is_player == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  const urlStr = '/playerInformation?mode=update' + '&player_id=' + playerId;
+                  router.push(urlStr);
+                }}
+                className='text-caption1'
+              >
+                選手情報更新
+              </MenuItem>
+            ) : ''}
+            {is_player == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  const urlStr = '/playerInformationRef' + '?player_id=' + playerId;
+                  router.push(urlStr);
+                  // router.push('/playerInformation?mode=create');
+                }}
+                className='text-caption1'
+              >
+                選手情報参照
+              </MenuItem>
+            ) : ''}
+            {is_player == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  const urlStr = '/playerInformationRef?mode=delete' + '&player_id=' + playerId;
+                  router.push(urlStr);
+                  // router.push('/playerInformation?mode=create');
+                }}
+                className='text-caption1'
+              >
+                選手情報削除
+              </MenuItem>
+            ) : ''}
+            {(is_administrator == 1 || is_jara == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/playerInformation?mode=create');
+                }}
+                className='text-caption1'
+              >
+                選手一括登録
+              </MenuItem>
+            ) : ''}
           </div>
         )}
         {/* clickIndexが2の時（団体押下時）に表示 */}
@@ -224,15 +275,17 @@ const Header: FC = () => {
             >
               団体検索
             </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/teamManagement');
-              }}
-              className='text-caption1'
-            >
-              団体管理
-            </MenuItem>
+            {(is_administrator == 1 || is_organization_manager == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/teamManagement');
+                }}
+                className='text-caption1'
+              >
+                団体管理
+              </MenuItem>
+            ) : ''}
             <MenuItem
               onClick={(e) => {
                 handleClose();
@@ -242,105 +295,124 @@ const Header: FC = () => {
             >
               団体登録
             </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/teamPlayerBulkRegister');
-              }}
-              className='text-caption1'
-            >
-              団体選手一括登録
-            </MenuItem>
+            {is_administrator == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/teamPlayerBulkRegister');
+                }}
+                className='text-caption1'
+              >
+                団体選手一括登録
+              </MenuItem>
+            ) : ''}
           </div>
         )}
         {/* clickIndexが3の時（ボランティア押下時）に表示 */}
         {clickIndex === 3 && (
           <div>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/volunteerSearch');
-              }}
-              className='text-caption1'
-            >
-              ボランティア検索
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                const urlStr = '/volunteerInformationRef' + '?volunteer_id=' + volunteerId;
-                router.push(urlStr);
-                // router.push('/volunteerInformationRef');
-              }}
-              className='text-caption1'
-            >
-              ボランティア情報参照
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                const urlStr = '/volunteerInformationRef?mode=delete' + '&volunteer_id=' + volunteerId;
-                router.push(urlStr);
-                // router.push('/volunteerSearch');
-              }}
-              className='text-caption1'
-            >
-              ボランティア情報削除
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/DummyMyPage');
-              }}
-              className='text-caption1'
-            >
-              ボランティア一括登録
-            </MenuItem>
+            {(is_administrator == 1 || is_jara == 1 || is_pref_boat_officer == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/volunteerSearch');
+                }}
+                className='text-caption1'
+              >
+                ボランティア検索
+              </MenuItem>
+            ) : ''}
+            {is_volunteer == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  const urlStr = '/volunteerInformationRef' + '?volunteer_id=' + volunteerId;
+                  router.push(urlStr);
+                  // router.push('/volunteerInformationRef');
+                }}
+                className='text-caption1'
+              >
+                ボランティア情報参照
+              </MenuItem>
+            ) : ''}
+            {is_volunteer == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  const urlStr = '/volunteerInformationRef?mode=delete' + '&volunteer_id=' + volunteerId;
+                  router.push(urlStr);
+                  // router.push('/volunteerSearch');
+                }}
+                className='text-caption1'
+              >
+                ボランティア情報削除
+              </MenuItem>
+            ) : ''}
+
+            {(is_administrator == 1 || is_jara == 1) ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/DummyMyPage');
+                }}
+                className='text-caption1'
+              >
+                ボランティア一括登録
+              </MenuItem>
+            ) : ''}
           </div>
         )}
         {/* clickIndexが4の時（その他押下時）に表示 */}
         {clickIndex === 4 && (
           <div>
+            {is_audience == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/userInformation?mode=update');
+                }}
+                className='text-caption1'
+              >
+                ユーザ情報更新
+              </MenuItem>
+            ) : ''}
+            {is_audience == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/userInformationRef');
+                }}
+                className='text-caption1'
+              >
+                ユーザ情報参照
+              </MenuItem>
+            ) : ''}
+            {is_audience == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/userInformationRef?mode=delete');
+                }}
+                className='text-caption1'
+              >
+                ユーザ情報削除
+              </MenuItem>
+            ) : ''}
+            {is_audience == 1 ? (
+              <MenuItem
+                onClick={(e) => {
+                  handleClose();
+                  router.push('/passwordchange');
+                }}
+                className='text-caption1'
+              >
+                パスワード変更
+              </MenuItem>
+            ) : ''}
             <MenuItem
               onClick={(e) => {
                 handleClose();
-                router.push('/userInformation?mode=update');
-              }}
-              className='text-caption1'
-            >
-              ユーザ情報更新
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/userInformationRef');
-              }}
-              className='text-caption1'
-            >
-              ユーザ情報参照
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/userInformationRef?mode=delete');
-              }}
-              className='text-caption1'
-            >
-              ユーザ情報削除
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                router.push('/passwordchange');
-              }}
-              className='text-caption1'
-            >
-              パスワード変更
-            </MenuItem>
-            <MenuItem
-              onClick={(e) => {
-                handleClose();
-                //ログアウト処理
+                //ログアウト処理 残件
               }}
               className='text-caption1'
             >
