9bb504d726c22347bd3a64a5fd0a7ac630a46587
h_makino <h_makino@dpt-inc.co.jp>
2024-05-08 10:41:30 +0900

    大会登録　重複チェック　試作

--- frontend/app/(Pages)/(App)/(Tournament)/tournament/page.tsx
+++ frontend/app/(Pages)/(App)/(Tournament)/tournament/page.tsx
@@ -210,7 +210,7 @@ export default function Tournaments() {
     ]);
 
     const raceNumberErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateRequired(row.race_number, 'レースNo.').length > 0;
@@ -227,32 +227,32 @@ export default function Tournaments() {
     //   return Validator.validateIntegerRange(row.entrysystem_race_id).length > 0;
     // });
     const raceNumberNegativeErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validatePositiveNumber(row.race_number).length > 0;
     });
     const eventIdErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateSelectRequired(row.event_id, '種目').length > 0;
     });
 
     const raceNameErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateRequired(row.race_name, 'レース名').length > 0;
     });
     const raceTypeErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateRequired(row.race_class_id, 'レース区分').length > 0;
     });
     const raceTypeNameErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       // console.log(row.race_class_name);
@@ -264,27 +264,27 @@ export default function Tournaments() {
     });
 
     const byGroupErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateRequired(row.by_group, '組別').length > 0;
     });
 
     const rangeErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateRequired(row.range, '距離').length > 0;
     });
     const rangeNegativeErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validatePositiveNumber(row.range).length > 0;
     });
 
     const startDateTimeErrorFlg = tableData.some((row) => {
-      if(row.checked){
+      if (row.checked) {
         return false; //削除チェックがされている場合、バリデーションを行わない 20240508
       }
       return Validator.validateRequired(row.start_date_time, '発艇日時').length > 0;
@@ -407,14 +407,19 @@ export default function Tournaments() {
     }
   };
 
-  // エントリーシステムレースIDの重複チェックを行う 20240506 
+  // エントリーシステムレースIDの重複チェックを行う 20240506
   const entrysystemRaceIdCehck = () => {
     var strArray = Array();
     if (mode == 'create') {
       console.log('create check');
       tableData.filter(
         (element, index, self) => (
-          self.findIndex((e) => e.entrysystem_race_id == element.entrysystem_race_id) != index
+          self.findIndex(
+            (e) =>
+              e.entrysystem_race_id == element.entrysystem_race_id &&
+              e.checked != true &&
+              element.checked != true,
+          ) != index
             ? self[index].entrysystem_race_id != '' && self[index].entrysystem_race_id != null
               ? strArray.push(
                   'エントリーシステムのレースIDが重複しています。' +
@@ -429,7 +434,12 @@ export default function Tournaments() {
       console.log('update check');
       tableData.filter(
         (element, index, self) => (
-          self.findIndex((e) => e.entrysystem_race_id == element.entrysystem_race_id) != index
+          self.findIndex(
+            (e) =>
+              e.entrysystem_race_id == element.entrysystem_race_id &&
+              e.checked != true &&
+              element.checked != true,
+          ) != index
             ? self[index].entrysystem_race_id != '' &&
               self[index].entrysystem_race_id != null &&
               self[index].checked != true
@@ -452,14 +462,17 @@ export default function Tournaments() {
       return false;
     }
   };
-  // レースNo.の重複チェックを行う 20240506 
+  // レースNo.の重複チェックを行う 20240506
   const raceNumberDuplicatCheck = () => {
     var strArray = Array();
     if (mode == 'create') {
       console.log('create check');
       tableData.filter(
         (element, index, self) => (
-          self.findIndex((e) => e.race_number == element.race_number) != index
+          self.findIndex(
+            (e) =>
+              e.race_number == element.race_number && e.checked != true && element.checked != true,
+          ) != index
             ? self[index].race_number != '' && self[index].race_number != null
               ? strArray.push('レースNo.が重複しています。' + self[index].race_number.toString())
               : null
@@ -471,7 +484,10 @@ export default function Tournaments() {
       console.log('update check');
       tableData.filter(
         (element, index, self) => (
-          self.findIndex((e) => e.race_number == element.race_number) != index
+          self.findIndex(
+            (e) =>
+              e.race_number == element.race_number && e.checked != true && element.checked != true,
+          ) != index
             ? self[index].race_number != '' &&
               self[index].race_number != null &&
               self[index].checked != true
@@ -1049,10 +1065,10 @@ export default function Tournaments() {
           <TextField
             type={'text'}
             value={row.entrysystem_race_id}
-            onChange={(e) => (
+            onChange={(e) =>
               // console.log(tableData),
               handleInputChangeRace(row.id, 'entrysystem_race_id', e.target.value)
-            )}
+            }
             className='my-[8px]'
             inputProps={{ maxLength: 8 }}
           />
