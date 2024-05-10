5ac39fcac63cfdbeb9a2d3c82f3507d759f6d964
h_makino <h_makino@dpt-inc.co.jp>
2024-05-10 09:55:23 +0900

    選手参照　スクロールバー　試作

--- frontend/app/(Pages)/(App)/(Player)/playerInformationRef/page.tsx
+++ frontend/app/(Pages)/(App)/(Player)/playerInformationRef/page.tsx
@@ -453,7 +453,7 @@ export default function PlayerInformationRef() {
                 </div>
               </div>
             </div>
-            <div className='overflow-auto h-[467px]'>
+            <div className='overflow-auto h-[267px]'>
               {/* 出漕結果情報一覧テーブル表示 */}
               <CustomTable>
                 {/* テーブルヘッダー */}
