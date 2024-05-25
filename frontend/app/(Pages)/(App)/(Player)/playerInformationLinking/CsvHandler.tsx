c5ffa57f631debc8a514d7290c2a55de2875522c
h_makino <h_makino@dpt-inc.co.jp>
2024-05-25 15:20:16 +0900

    選手連携　CSV読み込み　修正

--- frontend/app/(Pages)/(App)/(Player)/playerInformationLinking/CsvHandler.tsx
+++ frontend/app/(Pages)/(App)/(Player)/playerInformationLinking/CsvHandler.tsx
@@ -172,38 +172,36 @@ const CsvHandler = forwardRef<Handler, Props>(function FileUploader(props, ref) 
       <div className='flex flex-col gap-[10px] w-full'>
         <CustomInputLabel label={props.csvUploadProps.label}></CustomInputLabel>
         <div className='flex flex-row gap-[4px]'>
-          {!props.csvUploadProps.readonly && (
-            <div className='flex flex-col gap-[10px] w-full'>
-              <div {...getRootProps()} className=''>
-                <div className=''>
-                  <input {...getInputProps()} />
-                  <p className='text-secondaryText text-sm'>
-                    {isDragReject ? 'このファイル形式のアップロードは許可されていません。' : ''}
-                  </p>
-                  <div className='flex flex-col gap-[10px] w-full'>
-                    <div className='flex flex-row gap-[4px]'>
-                      <CustomTextField
-                        placeholder={'ここにファイルをドラッグ＆ドロップしてアップロード'}
-                        value={currentShowFile?.isUploaded ? currentShowFile?.file.name : ''}
-                        className='w-[450px] h-12'
-                      ></CustomTextField>
-                      <CustomButton
-                        onClick={() => {
-                          // TODO: ファイル参照処理
-                        }}
-                        className='w-[100px] h-[57px]'
-                      >
-                        参照
-                      </CustomButton>
-                    </div>
+          <div className='flex flex-col gap-[10px] w-full'>
+            <div {...getRootProps()} className=''>
+              <div className=''>
+                <input {...getInputProps()} />
+                <p className='text-secondaryText text-sm'>
+                  {isDragReject ? 'このファイル形式のアップロードは許可されていません。' : ''}
+                </p>
+                <div className='flex flex-col gap-[10px] w-full'>
+                  <div className='flex flex-row gap-[4px]'>
+                    <CustomTextField
+                      placeholder={'ここにファイルをドラッグ＆ドロップしてアップロード'}
+                      value={currentShowFile?.isUploaded ? currentShowFile?.file.name : ''}
+                      className='w-[450px] h-12'
+                    ></CustomTextField>
+                    <CustomButton
+                      onClick={() => {
+                        // TODO: ファイル参照処理
+                      }}
+                      className='w-[100px] h-[57px]'
+                    >
+                      参照
+                    </CustomButton>
                   </div>
                 </div>
               </div>
-              <div>
-                {isDragAccept ? 'ファイルをアップロードします。' : isDragReject ? 'エラー' : ''}
-              </div>
             </div>
-          )}
+            <div>
+              {isDragAccept ? 'ファイルをアップロードします。' : isDragReject ? 'エラー' : ''}
+            </div>
+          </div>
           {!props.csvUploadProps.readonly && (
             <CustomButton
               buttonType='primary'
