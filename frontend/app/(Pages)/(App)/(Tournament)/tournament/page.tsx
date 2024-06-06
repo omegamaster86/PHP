20c5e2536bc6b5eb8e05f6f2095e12e5a4ff567c
h_makino <h_makino@dpt-inc.co.jp>
2024-06-06 14:42:35 +0900

    大会登録画面　エントリーシステム大会ID　修正

--- frontend/app/(Pages)/(App)/(Tournament)/tournament/page.tsx
+++ frontend/app/(Pages)/(App)/(Tournament)/tournament/page.tsx
@@ -168,7 +168,7 @@ export default function Tournaments() {
 
     //エントリーシステムの大会ID用エラーメッセージ 20240606
     const entrysystemTournIdError = Validator.getErrorMessages([
-      Validator.validateAlphabetNumber(tournamentFormData.entrysystem_tourn_id, 'エントリーシステムの大会ID'),
+      tournamentFormData.entrysystem_tourn_id.length > 0 ? Validator.validateAlphabetNumber(tournamentFormData.entrysystem_tourn_id, 'エントリーシステムの大会ID') : '',
     ]);
 
     const tournNameError = Validator.getErrorMessages([
