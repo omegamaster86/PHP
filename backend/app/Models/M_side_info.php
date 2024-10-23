fef98ec027aca86084cabb67b364bacf52f0f179
牧野 仁志 <h_makino@dpt-inc.co.jp>
2024-10-23 04:10:13 +0000

    マイページ 選手プロフィール サイド情報 修正 マージ

--- backend/app/Models/M_side_info.php
+++ backend/app/Models/M_side_info.php
@@ -36,4 +36,32 @@ class M_side_info extends Model
         );
         return $result;
     }
+
+    //マイページ サイド情報 20241021
+    public function getMyPageSideInfo($side)
+    {
+        $result = DB::select(
+            'select 
+            CONCAT(abbr_name, "(", side_name, ")") as sideName,
+            case 
+                when side_code = 8 then ?
+                when side_code = 4 then ?
+                when side_code = 2 then ?
+                when side_code = 1 then ?
+                else null
+            end as isEnable
+            from m_side_info 
+            where 1=1
+            and delete_flag = 0
+            and side_code IN (8, 4, 2, 1)
+            order by display_order',
+            [
+                substr($side, 4, 1),
+                substr($side, 5, 1),
+                substr($side, 6, 1),
+                substr($side, 7, 1)
+            ]
+        );
+        return $result;
+    }
 }
