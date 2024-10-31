5294641c490bea6cc95c018015d8e960c3bdd15d
h_makino <h_makino@dpt-inc.co.jp>
2024-10-31 09:51:59 +0900

    【選手フォロー機能】【プロフィール 大会情報画面】BE 修正

--- backend/app/Models/T_tournaments.php
+++ backend/app/Models/T_tournaments.php
@@ -608,26 +608,18 @@ class T_tournaments extends Model
                                                 or exists (
                                                     select 1
                                                         FROM `t_race_result_record` 
+                                                        left join `t_followed_players` 
+													    on `t_race_result_record`.`player_id` = `t_followed_players`.`player_id`
                                                         WHERE 1=1
                                                         and `t_tournaments`.`tourn_id` = `t_race_result_record`.`tourn_id`
                                                         and `t_race_result_record`.delete_flag = 0
-                                                        and `t_race_result_record`.player_id = ?
-                                                )
-                                                or exists (
-                                                    select 1
-                                                        FROM `t_race_result_record` 
-                                                        WHERE 1=1
-                                                        and `t_tournaments`.`tourn_id` = `t_race_result_record`.`tourn_id`
-                                                        and `t_race_result_record`.delete_flag = 0
-                                                        and exists (
-															select 1
-																FROM `t_followed_players` 
-                                                                left join `t_race_result_record`
-																on `t_followed_players`.`player_id` = `t_race_result_record`.`player_id`
-																WHERE 1=1
-																and `t_followed_players`.delete_flag = 0
-																and `t_followed_players`.user_id = 2
-														)
+                                                        and (
+                                                            `t_race_result_record`.player_id = ?
+                                                            or (
+                                                                `t_followed_players`.delete_flag = 0
+														        and `t_followed_players`.user_id = ?
+                                                            )
+                                                        )
                                                 )
                                             )
                                             and `t_tournaments`.`delete_flag` = 0
