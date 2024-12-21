754abce79b978fe2fde500b28babba2a85c1df57
相田元気 <g.aida@genai.co.jp>
2024-12-21 14:43:46 +0000

    環境ごとにenvを呼び分ける

--- frontend/app/lib/axios.ts
+++ frontend/app/lib/axios.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosInstance } from 'axios';
 
 const axios: AxiosInstance = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000",
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
