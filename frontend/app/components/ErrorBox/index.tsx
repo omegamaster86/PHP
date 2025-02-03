3aa26e6b3636902e2b407841b14f331d73df2981
相田元気 <g.aida@genai.co.jp>
2025-02-03 08:32:19 +0000

    #572 削除した大会もURLでIDを指定すると表示できてしまう（大会情報は何も記載されていない）

--- frontend/app/components/ErrorBox/index.tsx
+++ frontend/app/components/ErrorBox/index.tsx
@@ -1,17 +1,14 @@
 export default function ErrorBox({ errorText }: { errorText: string[] }) {
+  if (errorText.length === 0) return null;
   return (
-    <>
-      {errorText?.length > 0 && (
-        <div className='p-2 bg-systemErrorBg border-systemErrorText border-solid border-[1px] text-center'>
-          {errorText?.map((message) => {
-            return (
-              <p key={message} className='text-systemErrorText text-small'>
-                {message}
-              </p>
-            );
-          })}
-        </div>
-      )}
-    </>
+    <div className='p-2 bg-systemErrorBg border-systemErrorText border-solid border-[1px] text-center'>
+      {errorText.map((message) => {
+        return (
+          <p key={message} className='text-systemErrorText text-small'>
+            {message}
+          </p>
+        );
+      })}
+    </div>
   );
 }
