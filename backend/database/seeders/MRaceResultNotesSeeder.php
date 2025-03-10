a1b6e5e26c5f34c8421c992e0381a531920c0ba4
相田元気 <g.aida@genai.co.jp>
2025-03-10 06:21:08 +0000

    初期投入データ修正

--- backend/database/seeders/MRaceResultNotesSeeder.php
+++ backend/database/seeders/MRaceResultNotesSeeder.php
@@ -13,7 +13,18 @@ class MRaceResultNotesSeeder extends Seeder
     public function run(): void
     {
         DB::statement("
-            SELECT 1;
+            INSERT INTO m_race_result_notes (race_result_notes_id, race_result_notes, display_order, registered_time, registered_user_id, updated_time, updated_user_id, delete_flag) VALUES 
+            (0, '', 1, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (50, '最下位位置', 2, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (60, 'BUW（艇重量不足）', 3, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (65, '再レース', 4, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (70, '除外', 5, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (80, 'DNS（棄権）', 6, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (81, 'DNS（放棄）', 7, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (85, 'DNF（途中棄権）', 8, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (90, '失格', 9, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0),
+            (95, 'ＯＰＥＮ', 10, CURRENT_TIMESTAMP, 0, CURRENT_TIMESTAMP, 0, 0)
+            ;
             ");
     }
 }
