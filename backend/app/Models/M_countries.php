081908d8a8a99dd831a592a7af2e13500c3eddfa
omegamaster86 <masya868686@gmail.com>
2025-05-24 22:55:49 +0900

    update

--- backend/app/Models/M_countries.php
+++ backend/app/Models/M_countries.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\DB;
 class M_countries extends Model
 {
     use HasFactory;
-
     //テーブルがm_prefecturesと結びつくように指定する
     protected $table = 'm_countries';
     protected $primaryKey = 'country_id';
@@ -33,11 +32,11 @@ class M_countries extends Model
     {
         $countries = DB::select(
             'select `country_id`,
-                                    `country_code`,
-                                    `country_name`
-                                    from `m_countries`
-                                    where `delete_flag`=0
-                                    order by `display_order`'
+                `country_code`,
+                `country_name`
+                from `m_countries`
+                where `delete_flag`=0
+                order by `display_order`'
         );
         return $countries;
     }
