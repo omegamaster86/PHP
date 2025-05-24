2a74bafaeb9fd05853ac95d55759c21e23ccac5a
omegamaster86 <masya868686@gmail.com>
2025-05-24 22:56:01 +0900

    update

--- backend/app/Models/M_disability_type.php
+++ backend/app/Models/M_disability_type.php
@@ -14,8 +14,7 @@ class M_disability_type extends Model
     public function getDisabilityType()
     {
         $result = DB::select(
-            '
-            select
+            'select
                 dis_type_id
                 , dis_type_name
                 , display_order
@@ -28,8 +27,7 @@ class M_disability_type extends Model
                 jara_new_pf.m_disability_type 
             where
                 delete_flag = ?
-            order by display_order
-            ',
+            order by display_order',
             [0]
         );
         return $result;
